332b7646ef7f941e85d65a6b29219fbc4258ac09
Hao <zhouhaomuc@gmail.com>
2017-01-23 20:19:11 -0500

    a new method to solve three sum

--- Code/ThreeSum.java
+++ Code/ThreeSum.java
@@ -3,33 +3,29 @@ import java.util.*;
 
 public class ThreeSum {
 	public static void main(String[] args) {
-		int[] nums = {0,0,0,0};
+		int[] nums = {0,0,0};
 		List<List<Integer>> re = threeSum(nums);
 	}
 	public static List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> result = new LinkedList<>();
-        for(int i=0; i<nums.length; i++){
-            if((i>0)&&(nums[i] == nums[i-1])) continue;
-            int num = nums[i];
-            int target = -num;
-            HashMap<Integer, Integer> dic= new HashMap<Integer, Integer>();
-            for(int j=i+1; j<nums.length; j++){
-                if((j>i+2)&&(nums[j] == nums[j-1]) ){
-                    if((dic.containsKey(target - nums[j]) && (target/2)!=nums[j]) || ((j>i+2)&&(nums[j] == nums[j-1])&&(nums[j] == nums[j-2]))){
-                        continue;
-                    }
-                    
-                }
-                if(dic.containsKey(target - nums[j])){
-                    List<List<Integer>> temp = new LinkedList<>();
-                    temp.add(Arrays.asList(nums[i], nums[j], (target - nums[j])));
-                    result.addAll(temp);
-                    dic.put(nums[j], j);
-                }else{
-                	dic.put(nums[j], j);
-                }
-            }
+        for(int i=0; i<nums.length-2; i++){
+            if((i>0) && (nums[i] == nums[i-1])) continue;
+        	else{
+        	    int target = -nums[i];
+        	    int low = i + 1;
+        	    int high = nums.length - 1;
+        		while(low < high){
+        			if((nums[low] + nums[high]) == target){
+        				result.add(Arrays.asList(nums[i], nums[low], nums[high]));
+        				low ++;
+        				high --;
+        				while((nums[low] == nums[low -1]) && (low<high)) low++;
+        				while((nums[high] == nums[high+1]) && (low<high)) high--;
+        			}else if((nums[low] + nums[high])>target) high --;
+        			else low ++;
+        		}
+        	}
         }
     return result;
     }
